22f5ac5b2ae0472489e0ca2b9082b968fdf2f699
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-04-24 21:41:27 +0100

    added new color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'spotify-green': '#18D860',
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
-};
+}
