a0e503f801ee63d5726cc54e0a477049bc7c07f0
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-04-24 14:37:33 +0100

    new tailwind class

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
