dd525460d9430e35ac6c77d6613a3620a835d60a
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-04-24 21:42:02 +0100

    moved some fixed components to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,36 @@
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+import { getSession, SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
+import Sidebar from '../components/sidebar';
+import Player from '../components/Player';
+import Profile from '../components/Profile';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
-        <Component {...pageProps} />
+        <div className="bg-black">
+          <main className="flex">
+            <Sidebar />
+            <Profile />
+            <Component {...pageProps} />
+          </main>
+          <div className="sticky bottom-0">
+            <Player />
+          </div>
+        </div>
       </RecoilRoot>
     </SessionProvider>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: { session },
+  };
 }
 
 export default MyApp;
