b211801860f248dcb79b4337826d424b8d566ac5
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-04-24 21:43:11 +0100

    updated data access

--- components/song.tsx
+++ components/song.tsx
@@ -11,33 +11,29 @@ function Song({ order, track }) {
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
 
   const playSong = async () => {
-    setCurrentTrackId(track.track.id);
+    setCurrentTrackId(track.id);
     setIsPlaying(true);
     spotifyApi.play({
-      uris: [track.track.uri],
+      uris: [track.uri],
     });
   };
 
   return (
     <div
       onClick={playSong}
-      className="grid cursor-pointer grid-cols-2 rounded-md py-4 px-5 text-gray-500 transition-all hover:bg-gray-900"
+      className="grid cursor-pointer grid-cols-2 rounded-sm py-2 px-5 text-gray-500 transition-all hover:bg-gray-900"
     >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
-        <img
-          src={track.track.album.images[0].url}
-          className="h-10 w-10"
-          alt=""
-        />
+        <img src={track.album.images[0].url} className="h-10 w-10" alt="" />
         <div>
-          <p className="w-36 truncate text-white lg:w-64">{track.track.name}</p>
-          <p className="w-40">{track.track.artists[0].name}</p>
+          <p className="w-36 truncate text-white lg:w-64">{track.name}</p>
+          <p className="w-36 truncate lg:w-64">{track.artists[0].name}</p>
         </div>
       </div>
       <div className="ml-auto flex items-center justify-between md:ml-0">
-        <p className="hidden w-40 md:inline">{track.track.album.name}</p>
-        <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
+        <p className="hidden w-40 md:inline">{track.album.name}</p>
+        <p>{millisToMinutesAndSeconds(track.duration_ms)}</p>
       </div>
     </div>
   );
--- components/songs.tsx
+++ components/songs.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { playlistState } from '../atoms/playlistAtom';
 import Song from './song';
 
-function Songs() {
-  const playlist = useRecoilValue(playlistState);
-
-  console.log(playlist);
-
+function Songs({ songs }) {
   return (
     <div className="flex flex-col space-y-1 px-8 pb-28 text-white">
-      {playlist?.tracks.items.map((song: any, index) => (
-        <Song key={song.track.id} track={song} order={index} />
+      {songs?.map((song: any, index) => (
+        <Song key={song.id} track={song} order={index} />
       ))}
     </div>
   );
