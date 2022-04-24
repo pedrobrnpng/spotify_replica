7115ae63e60f96950259da49620c6b8001324c72
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-04-24 14:39:17 +0100

    added button functionality and time formatting

--- components/song.tsx
+++ components/song.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import useSpotify from '../hooks/useSpotify';
+import { millisToMinutesAndSeconds } from '../lib/time';
 
 function Song({ order, track }) {
   const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
 
-  console.log(track);
+  const playSong = async () => {
+    setCurrentTrackId(track.track.id);
+    setIsPlaying(true);
+    spotifyApi.play({
+      uris: [track.track.uri],
+    });
+  };
 
   return (
-    <div className="grid grid-cols-2">
+    <div
+      onClick={playSong}
+      className="grid cursor-pointer grid-cols-2 rounded-md py-4 px-5 text-gray-500 transition-all hover:bg-gray-900"
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
@@ -16,13 +31,13 @@ function Song({ order, track }) {
           alt=""
         />
         <div>
-          <p>{track.track.name}</p>
-          <p>{track.track.artists[0].name}</p>
+          <p className="w-36 truncate text-white lg:w-64">{track.track.name}</p>
+          <p className="w-40">{track.track.artists[0].name}</p>
         </div>
       </div>
       <div className="ml-auto flex items-center justify-between md:ml-0">
-        <p className="hidden md:inline">{track.track.album.name}</p>
-        <p>3:50</p>
+        <p className="hidden w-40 md:inline">{track.track.album.name}</p>
+        <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
       </div>
     </div>
   );
