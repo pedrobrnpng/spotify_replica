a067ced31b43df568130fd690ffbc70e3c031b23
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-05-16 18:10:54 +0100

    helper function to get the time passed from date

--- lib/time.ts
+++ lib/time.ts
@@ -3,3 +3,14 @@ export function millisToMinutesAndSeconds(millis) {
   var seconds = ((millis % 60000) / 1000).toFixed(0);
   return minutes + ":" + (Number(seconds) < 10 ? '0' : '') + seconds;
 }
+
+export function timePassed(time) {
+  const currentTime = new Date().getTime();
+  const last_played = new Date(time).getTime();
+  const timePassed = currentTime - last_played;
+  const minutes = Math.floor(timePassed / 60000);
+  const seconds = Math.floor((timePassed % 60000) / 1000);
+
+  if (minutes > 0) return `${minutes}m`;
+  return `${seconds}s`;
+};
